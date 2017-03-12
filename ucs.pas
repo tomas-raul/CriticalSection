0621953f031d482e79190403037c8a56beb1fc52
tomas-raul <tomas.emresz@strankysnadno.cz>
2017-03-12 21:54:10 +0100

    Initial commit

---
+++ ucs.pas
@@ -0,0 +1,128 @@
+unit uCS;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  syncobjs;
+
+type
+
+    { tIQCriticalSection }
+
+    tIQCriticalSection = class(tCriticalSection)
+    private
+      class var FLogLonger: Integer;
+      class var FTimeOutException: Integer;
+    private
+      FLockName: String;
+      procedure SetLockName(AValue: String);
+    public
+      constructor Create;
+      procedure Enter (const pLockName : string);
+      procedure Leave;
+
+      class property LogLonger : Integer read FLogLonger write fLogLonger;
+      class property TimeOutException : Integer read FTimeOutException write fTimeOutException;
+
+    published
+       property LockName : String read FLockName write SetLockName;
+    end;
+
+    tCS = tIQCriticalSection;
+
+{$MACRO ON}
+{$I uCSmacro.inc}
+{$DEFINE NewSync}
+
+function InitCS : tCS;
+procedure EnterCS( CS : tCS; const pLockName : string);
+procedure LeaveCS( CS : tCS);
+procedure DoneCS( CS : tCS);
+
+implementation
+
+uses
+  DateUtils
+{$IFDEF LOG},uLog{$ENDIF}
+  ;
+
+function InitCS: tCS;
+begin
+   result := tIQCriticalSection.Create;
+end;
+
+procedure EnterCS( CS: tCS; const pLockName : string);
+begin
+   CS.Enter(pLockName);
+end;
+
+procedure LeaveCS( CS: tCS);
+begin
+   CS.Leave;
+end;
+
+procedure DoneCS( CS: tCS);
+begin
+   CS.Free;
+end;
+
+{ tIQCriticalSection }
+
+constructor tIQCriticalSection.Create;
+begin
+   inherited;
+   LogLonger := 50;
+   LockName := '';
+   TimeOutException := 5000;
+end;
+
+procedure tIQCriticalSection.Enter(const pLockName: string);
+var start, LogAfter, TimeOutAfter : tDateTime;
+    logafterdone : boolean;
+begin
+{$IFDEF NewSync}
+   start := now;
+   LogAfter := DateUtils.IncMilliSecond(start,fLogLonger);
+   TimeOutAfter := DateUtils.IncMilliSecond(start,fTimeOutException);
+   logafterdone := false;
+
+   while not TryEnter do
+   begin
+     if ( now > TimeOutAfter ) then
+     begin
+        {$IFDEF LOG}        Log('CS-Timeout - ' + pLockName + ' waits for ' + fLockName + ' longer than ' + IntToStr(TimeOutException) + ' ms');{$ENDIF}
+        raise Exception.Create('CS-Timeout - ' + pLockName + ' waits for ' + fLockName + ' longer than ' + IntToStr(TimeOutException) + ' ms');
+     end;
+     if ( not logafterdone and (now > LogAfter) ) then
+     begin
+        logafterdone := true;
+     end;
+   end;
+   if (logafterdone) then
+   begin
+      {$IFDEF LOG}      Log('CS-Wait-done - ' + pLockName + ' waits for ' + fLockName + ' ' + IntToStr(DateUtils.MilliSecondsBetween(start,now)) + ' ms');{$ENDIF}
+   end;
+
+   FLockName:=pLockName;
+{$ELSE}
+   inherited Enter;
+{$ENDIF}
+end;
+
+procedure tIQCriticalSection.Leave;
+begin
+   inherited Leave;
+end;
+
+procedure tIQCriticalSection.SetLockName(AValue: String);
+begin
+  if FLockName=AValue then Exit;
+  FLockName:=AValue;
+end;
+
+end.
+
